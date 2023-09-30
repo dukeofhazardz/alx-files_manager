e9f2806a71385401f52f2dbb487da1b1d5f3db89
Daniel John <dj71286@gmail.com>
2023-09-30 17:56:02 +0100

    Fixed errors in user controller

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,14 +7,14 @@ const UserController = {
     const { email, password } = req.body;
 
     if (!email) {
-      res.status(400).json({ error: 'Missing email' });
+      return res.status(400).json({ error: 'Missing email' });
     }
     if (!password) {
-      res.status(400).json({ error: 'Missing password' });
+      return res.status(400).json({ error: 'Missing password' });
     }
     const existingUser = await dbClient.client.db(dbClient.database).collection('users').findOne({ email });
     if (existingUser) {
-      res.status(400).json({ error: 'Already exist' });
+      return res.status(400).json({ error: 'Already exist' });
     }
 
     const hashedPw = sha1(password);
